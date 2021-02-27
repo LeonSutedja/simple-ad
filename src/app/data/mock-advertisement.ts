40f69d1ef5824f2c9acacc9ad6c22fc793a0af35
Leon Sutedja <leon.sutedja@gmail.com>
2021-02-28 06:08:58 +1100

    Change advertisement from an interface to class.

--- src/app/data/mock-advertisement.ts
+++ src/app/data/mock-advertisement.ts
@@ -1,7 +1,7 @@
 import { Advertisement } from '../model/advertisement';
 
 export const ADVERTISEMENTS: Advertisement[] = [
-  { id: 11, name: 'Classic Ad', description: 'Offers the most basic level of advertisement', price: 269.99 },
-  { id: 12, name: 'Stand out Ad', description: 'Allows advertisers to use a company logo and use a longer presentation text', price: 322.99 },
-  { id: 13, name: 'Premium Ad', description: 'Same benefits as Standout Ad, but also puts the advertisement at the top of the results, allowing higher visibility', price: 394.99 }
+  Advertisement.new(11, 'Classic Ad', 'Offers the most basic level of advertisement', 269.99),
+  Advertisement.new(12, 'Stand out Ad', 'Allows advertisers to use a company logo and use a longer presentation text', 322.99),
+  Advertisement.new(13, 'Premium Ad', 'Same benefits as Standout Ad, but also puts the advertisement at the top of the results, allowing higher visibility', 394.99),
 ];
--- src/app/model/advertisement.ts
+++ src/app/model/advertisement.ts
@@ -1,6 +1,19 @@
-export interface Advertisement {
+export class Advertisement {
+  static new(id: number, name: string, description: string, price: number) : Advertisement
+  {
+    return new Advertisement(id, name, description, price);
+  }
+
   id: number;
   name: string;
   description: string;
   price: number;
+
+  private constructor(id: number, name: string, description: string, price: number)
+  {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
 }
